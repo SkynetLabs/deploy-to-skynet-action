453c35be452a6845368de85deec2cb8b349cf0b2
Daniel Helm <dan.helm@gmail.com>
2021-11-03 16:36:13 -0500

    remove skynet-js dep, add cookie option

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
-const { SkynetClient: NodeSkynetClient } = require("@skynetlabs/skynet-nodejs");
-const { genKeyPairFromSeed, SkynetClient } = require("skynet-js");
+const core = require('@actions/core');
+const github = require('@actions/github');
+const {
+  SkynetClient,
+  genKeyPairFromSeed,
+} = require('@skynetlabs/skynet-nodejs');
 
 function outputAxiosErrorMessage(error) {
   if (error.response) {
@@ -11,23 +13,29 @@ function outputAxiosErrorMessage(error) {
     console.log(
       `${method.toUpperCase()} ${path} failed with status ${status}: ${statusText}`
     );
-    console.log(error.response.data || "Response contained no data");
+    console.log(error.response.data || 'Response contained no data');
   }
 }
 
 function prepareUploadOptions() {
   const options = {};
 
-  if (core.getInput("try-files")) {
+  if (core.getInput('try-files')) {
     // transform try-files input which is space separated list
     // of file paths into an array of those paths
-    options.tryFiles = core.getInput("try-files").split(/\s+/);
+    options.tryFiles = core.getInput('try-files').split(/\s+/);
   }
 
-  if (core.getInput("not-found-page")) {
+  if (core.getInput('not-found-page')) {
     // transform not-found-page input which is a single file path into
     // an object with a 404 key and its value being the specified path
-    options.errorPages = { 404: core.getInput("not-found-page") };
+    options.errorPages = { 404: core.getInput('not-found-page') };
+  }
+
+  if (core.getInput('skynet-jwt')) {
+    // transform not-found-page input which is a single file path into
+    // an object with a 404 key and its value being the specified path
+    options.cusomCookie = `skynet-jwt=${core.getInput('skynet-jwt')}`;
   }
 
   return options;
@@ -36,46 +44,45 @@ function prepareUploadOptions() {
 (async () => {
   try {
     // upload to skynet
-    const nodeClient = new NodeSkynetClient(core.getInput("portal-url"));
-    const skynetClient = new SkynetClient(core.getInput("portal-url"));
-    const skylink = await nodeClient.uploadDirectory(
-      core.getInput("upload-dir"),
+    const client = new SkynetClient(core.getInput('portal-url'));
+    const skylink = await client.uploadDirectory(
+      core.getInput('upload-dir'),
       prepareUploadOptions()
     );
 
     // generate base32 skylink url from base64 skylink
-    const skylinkUrl = await skynetClient.getSkylinkUrl(skylink, {
+    const skylinkUrl = await client.getSkylinkUrl(skylink, {
       subdomain: true,
     });
 
-    core.setOutput("skylink", skylink);
+    core.setOutput('skylink', skylink);
     console.log(`Skylink: ${skylink}`);
 
-    core.setOutput("skylink-url", skylinkUrl);
+    core.setOutput('skylink-url', skylinkUrl);
     console.log(`Deployed to: ${skylinkUrl}`);
 
     // if registry is properly configured, update the skylink in the entry
-    if (core.getInput("registry-seed") && core.getInput("registry-datakey")) {
+    if (core.getInput('registry-seed') && core.getInput('registry-datakey')) {
       try {
-        const seed = core.getInput("registry-seed");
-        const dataKey = core.getInput("registry-datakey");
+        const seed = core.getInput('registry-seed');
+        const dataKey = core.getInput('registry-datakey');
         const { publicKey, privateKey } = genKeyPairFromSeed(seed);
 
         const [entryUrl, resolverSkylink] = await Promise.all([
-          skynetClient.registry.getEntryUrl(publicKey, dataKey),
-          skynetClient.registry.getEntryLink(publicKey, dataKey),
-          skynetClient.db.setDataLink(privateKey, dataKey, skylink),
+          client.registry.getEntryUrl(publicKey, dataKey),
+          client.registry.getEntryLink(publicKey, dataKey),
+          client.db.setDataLink(privateKey, dataKey, skylink),
         ]);
-        const resolverUrl = await skynetClient.getSkylinkUrl(resolverSkylink, {
+        const resolverUrl = await client.getSkylinkUrl(resolverSkylink, {
           subdomain: true,
         });
 
         console.log(`Registry entry updated: ${entryUrl}`);
 
-        core.setOutput("resolver-skylink-url", resolverUrl);
+        core.setOutput('resolver-skylink-url', resolverUrl);
         console.log(`Resolver Skylink Url: ${resolverUrl}`);
 
-        core.setOutput("resolver-skylink", resolverSkylink);
+        core.setOutput('resolver-skylink', resolverSkylink);
         console.log(`Resolver Skylink: ${resolverSkylink}`);
       } catch (error) {
         outputAxiosErrorMessage(error);
@@ -86,7 +93,7 @@ function prepareUploadOptions() {
 
     // put a skylink in a pull request comment if available
     if (github.context.issue.number) {
-      const gitHubToken = core.getInput("github-token");
+      const gitHubToken = core.getInput('github-token');
       const octokit = github.getOctokit(gitHubToken);
 
       try {
