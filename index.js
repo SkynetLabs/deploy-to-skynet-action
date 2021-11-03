a41b90e13a4b2b21db4640281ddbd5c32c7da44f
Daniel Helm <dan.helm@gmail.com>
2021-11-03 17:27:37 -0500

    typo on customCookie

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function prepareUploadOptions() {
   if (core.getInput('skynet-jwt')) {
     // transform not-found-page input which is a single file path into
     // an object with a 404 key and its value being the specified path
-    options.cusomCookie = `skynet-jwt=${core.getInput('skynet-jwt')}`;
+    options.customCookie = `skynet-jwt=${core.getInput('skynet-jwt')}`;
   }
 
   return options;
